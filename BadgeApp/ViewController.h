b4fdb5b99070b87da96868c91b26c5c97c891ef4
mrkingchan <1203710130@qq.com>
2018-09-26 11:41:54 +0800

    Initial Commit

---
+++ BadgeApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BadgeApp
+//
+//  Created by Macx on 2018/9/26.
+//  Copyright © 2018年 Chan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
